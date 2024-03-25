fe6150f22daa8924ef3060a1e199dde3e2a36824
NajeebJaba <najeebshj@hotmail.com>
2024-03-25 23:34:49 +0200

    api profile page

--- logic/api_logic/api_profile_page.py
+++ logic/api_logic/api_profile_page.py
@@ -1,10 +1,22 @@
-from infra.api_infra.api_browser_wrapper import APIBrowserWrapper
+from infra.api_infra.api_wrapper import APIWrapper
 
-class APIProfilePage:
-    def __init__(self, api_browser_wrapper):
-        self.api_browser_wrapper = api_browser_wrapper
 
-    def update_profile(self, user_id, profile_data, headers):
-        endpoint = f"api/users/{user_id}/profile/update"
-        response = self.api_browser_wrapper.put(endpoint, profile_data, headers)
-        return response
+class ProfilePage(APIWrapper):
+    def __init__(self):
+        super().__init__()
+        self.json = None
+
+    def profile_page_refresh(self, user_id, user_name, user_mail, user_password):
+        self.json_data = {"id": user_id, "name": user_name,
+                          "email": user_mail, "password": user_password}
+
+        print(f"About to update profile for user ID: {user_id}")
+        print(f"Updating to: Name - {user_name}, Email - {user_mail}")
+
+        self.response = self.request.put(f'{self.url[:-2]}api/users/profile/update/', json=self.json_data,
+                                         headers=self.headersUpdate)
+
+        print(f'Profile updated. Response status code: {self.response.status_code}')
+        print(f'Updated data: {self.response.json()}')
+
+        return self.response
