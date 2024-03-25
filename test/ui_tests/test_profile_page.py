719fbc2f10afb732c30c94d63c3867658d7b3c96
NajeebJaba <najeebshj@hotmail.com>
2024-03-25 23:47:55 +0200

    ui test profile page

--- test/ui_tests/test_profile_page.py
+++ test/ui_tests/test_profile_page.py
@@ -2,8 +2,6 @@ import unittest
 from infra.ui_infra.browser_wrapper import BrowserWrapper
 from logic.ui_logic.base_page.home_page import HomePage
 from logic.ui_logic.login_page import LoginPage
-from logic.ui_logic.register_page import RegisterPage
-from logic.ui_logic.profile_page import ProfilePage
 from infra.ui_infra.config_handler import ConfigHandler
 import time
 
@@ -16,42 +14,26 @@ class TestProfilePage(unittest.TestCase):
         cls.driver = cls.browser_wrapper.initialize_driver()
         cls.home_page = HomePage(cls.driver)
         cls.login_page = LoginPage(cls.driver)
-        cls.register_page = RegisterPage(cls.driver)
-        cls.profile_page = ProfilePage(cls.driver)
 
     def setUp(self):
         self.driver.get(self.config['url'])
+        time.sleep(2)
+
+    def test_navigate_to_profile_page(self):
+        # click on login button
         self.home_page.click_on_sign_in_link()
-        time.sleep(3)
-        self.login_page.click_register()
+        time.sleep(2)
+
+        # Enter username and password then click signin
+        self.login_page.enter_username(self.config["email"])
+        self.login_page.enter_password_and_login(self.config["password"])
         time.sleep(3)
 
-    def test_profile_update(self):
-        self.driver.get(self.config['url'])
-        self.home_page.click_on_sign_in_link()
-        time.sleep(3)
-
-        self.login_page.click_register()
-        time.sleep(3)
-
-        self.register_page.fill_registration_form(
-            name=self.config["register_name_for_profile"],
-            email=self.config["register_email_for_profile"],
-            password=self.config["register_password_for_profile"],
-            confirm_password=self.config["register_confirm_password_for_profile"]
-        )
-        time.sleep(3)
-        self.register_page.submit_registration()
-        time.sleep(3)
+        # from home page click on the dropdown and then click on profile button
         self.home_page.click_profile_button()
-        time.sleep(3)
-        self.profile_page.update_profile(
-            self.config["updating_name_on_profile"],
-            self.config["register_email_for_profile"],
-            self.config["register_password_for_profile"],
-            self.config["register_confirm_password_for_profile"]
-        )
-        time.sleep(3)
+
+        # in profile page, wait for 15 seconds and don't do anything
+        time.sleep(10)
 
     @classmethod
     def tearDownClass(cls):
