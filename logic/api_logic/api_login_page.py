311c8f8e29d1a3489aaf5b730c8b68845edd12ea
NajeebJaba <najeebshj@hotmail.com>
2024-03-26 19:52:01 +0200

    logic

--- logic/api_logic/api_login_page.py
+++ logic/api_logic/api_login_page.py
@@ -4,8 +4,21 @@ from infra.api_infra.api_wrapper import APIWrapper
 class LoginAPI(APIWrapper):
     def __init__(self):
         super().__init__()
+        self.json = None
 
-    def login_user(self, email, password):
-        login_payload = {"email": email, "password": password}
-        self.response = self.request.post(f'{self.url[:-2]}api/users/login/', json=login_payload)
-        return self.response.status_code
+    def login_with_credentials(self, email, password):
+        self.json = {
+            "username": email,
+            "password": password
+        }
+
+        print(f"Attempting to login with email: {email}")
+        self.response = self.request.post(f'{self.url[:-2]}api/users/login/', json=self.json)
+
+        if self.response.ok:
+            print("Login successful")
+        else:
+            print(f"Login failed with status code: {self.response.status_code}")
+            print(f"Response: {self.response.json()}")
+
+        return self.response
